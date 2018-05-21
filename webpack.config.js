7362598108afa6245accf955de544e9895a713ce
Maximo Belen <maximo.fassnacht@exozet.com>
2018-05-21 12:32:29 +0200

    remove unneed package

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
     entry: { main: './src/scripts/index.js' },
     module: {
