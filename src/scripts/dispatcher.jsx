1dcd3444b7455c6485d41a92009cadba3bffcef6
Maximo Fassnacht <maximo.fassnacht@exozet.com>
2018-05-24 18:11:22 +0200

    dispatcher

--- src/scripts/dispatcher.jsx
+++ src/scripts/dispatcher.jsx
@@ -1,14 +1,21 @@
-'use strict';
-
 import {Dispatcher} from 'flux';
+
 var AppDispatcher = new Dispatcher();
 
-AppDispatcher.handleViewAction = function(action) {
-  this.dispatch({
-    source: 'VIEW_ACTION',
-    action: action
-  });
+AppDispatcher.handleLoadAction = function(action) {
+    this.dispatch({
+        source: 'LOAD_ACTION',
+        action: action
+    });
 }
+
+AppDispatcher.handleViewAction = function(action) {
+    this.dispatch({
+        source: 'VIEW_ACTION',
+        action: action
+    });
+}
+
 AppDispatcher.handleServerAction = function(action) {
 	this.dispatch({
 		source: 'SERVER_ACTION',
