64f413930ac0209c9b0d67ca8d1614af3081c79a
Maximo Fassnacht <maximo.fassnacht@exozet.com>
2018-05-28 14:03:59 +0200

    trigger action add in timeline

--- src/scripts/components/Timeline/timeline.jsx
+++ src/scripts/components/Timeline/timeline.jsx
@@ -1,38 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TimelineComponent from 'react-visjs-timeline';
+import ActionsStore from '../../stores/actionsStore';
+import ActionsActions from '../../actions/viewActions/actionsActions';
+
+function getAllActionsFromStore() {
+    return ActionsStore.getAll()
+}
 
 class Timeline extends React.Component {
 
+	constructor(props) {
+		super(props);
+		var actions = getAllActionsFromStore();
+		var items = this.buildItems(actions);
+		this.state = {items: items};
+	}
+
+	buildItems(actions) {
+		var items = [{
+			start: -1,
+			end: -1,
+			content: ''
+		}];
+		for (var i = 0; i < actions.length; i++) {
+
+			var item = {
+				className: actions[i].type,
+				id:  actions[i].id,
+				start: 1,
+				end: 3,
+				content: actions[i].title
+			};
+
+			items.push(item);
+		}
+		return items;
+	}
+
+	onChange() {
+		var actions = getAllActionsFromStore();
+		var items = this.buildItems(actions);
+		this.setState({items: items});
+	}
+
 	componentDidMount() {
-		this.container = ReactDOM.findDOMNode(this);
+		ActionsStore.addChangeListener(this.onChange.bind(this));
+	}
+
+	componentWillUnmount() {
+		ActionsStore.removeChangeListener(this.onChange.bind(this));
 	}
 
 	render() {
 		const options = {
 			width: '100%',
-			height: '200px',
-			stack: false,
-			min: 0,
-			max: 100000,
-			start: 0,
-			end: 100,
-			showMajorLabels: true,
-			showCurrentTime: true,
-			zoomMin: 1000000,
-			type: 'background'
+  			height: '200px'
 		};
-
-		const items = [{
-			start: 1,
-			end: 3,
-			content: 'Trajectory A'
-		}]
-
 
 		return (
 			<div className="timeline">
-				<TimelineComponent items={items} options={options}></TimelineComponent>
+				<TimelineComponent  ref="timeline" items={this.state.items} options={options}></TimelineComponent>
 			</div>
 		);
 	}
