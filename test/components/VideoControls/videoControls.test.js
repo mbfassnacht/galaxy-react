a1bb6503978396328cb36b1fe50bc7b3e791ad8f
Maximo Fassnacht <maximo.fassnacht@exozet.com>
2018-05-29 15:57:24 +0200

    add video controls test

--- test/components/VideoControls/videoControls.test.js
+++ test/components/VideoControls/videoControls.test.js
@@ -1,13 +1,47 @@
 // videoControls.test.js
+jest.dontMock('../../../src/scripts/actions/viewActions/VideoActions.js');
+jest.dontMock('../../../src/scripts/stores/videoStatusStore.js');
+jest.mock('../../../src/scripts/dispatcher.jsx');
+
 import React from 'react';
 import VideoControls from '../../../src/scripts/components/VideoControls/videoControls.jsx';
-import VideoStatusStore from '../../../src/scripts/stores/videoStatusStore.js';
-
+import VideoActions from '../../../src/scripts/actions/viewActions/VideoActions.js';
 import { shallow, mount, render } from 'enzyme';
 
 describe('TESTING VIDEOCONTROLS COMPONENT', () => {
 
-    var statusStore = VideoStatusStore.getStatus();
+    var AppDispatcher;
+    var VideoStore;
+    var callback;
+    var statusStore;
+
+    var actionPlay = {
+        actionType: "PLAY_ACTION"
+    };
+
+    var actionPause = {
+        actionType: "PAUSE_ACTION",
+    };
+
+    var actionMute = {
+        actionType: "MUTE_ACTION",
+        mute: true
+    };
+
+    var actionIncrease = {
+        actionType: "INCREASE_ACTION"
+    };
+
+    var actionDecrease = {
+        actionType: "DECREASE_ACTION"
+    };
+
+    beforeEach(function() {
+        AppDispatcher = require('../../../src/scripts/dispatcher.jsx');
+        VideoStore = require('../../../src/scripts/stores/videoStatusStore.js').default;
+        callback = AppDispatcher.register.mock.calls[0][0];
+        statusStore = VideoStore.getStatus();
+    });
 
     it('should be selectable by class "video"', () => {
         expect(shallow(<VideoControls />).is('.video-controls')).toBe(true);
@@ -23,5 +57,56 @@ describe('TESTING VIDEOCONTROLS COMPONENT', () => {
         expect(VideoControlsComponent.state('mute')).toBe(statusStore.mute);
         expect(VideoControlsComponent.state('time')).toBe(statusStore.time);
         expect(VideoControlsComponent.state('duration')).toBe(statusStore.duration);
+	});
+
+    it('should dispatch pause action on click', () => {
+        const VideoControlsComponent = shallow(<VideoControls />);
+        VideoControlsComponent.instance().onPause();
+        callback(actionPause);
+
+        var status = VideoStore.getStatus();
+        expect(status.playing).toBe(false);
+	});
+
+    it('should dispatch play action on click', () => {
+        const VideoControlsComponent = shallow(<VideoControls />);
+        VideoControlsComponent.instance().onPlay();
+        callback(actionPlay);
+
+        var status = VideoStore.getStatus();
+        expect(status.playing).toBe(true);
+	});
+
+    it('should dispatch mute action on click', () => {
+        const VideoControlsComponent = shallow(<VideoControls />);
+        VideoControlsComponent.instance().toggleMute();
+        callback(actionMute);
+
+        var status = VideoStore.getStatus();
+        expect(status.mute).toBe(true);
+	});
+
+    it('should increase time after forward action clicked', () => {
+        const VideoControlsComponent = shallow(<VideoControls />);
+
+        var oldTime = VideoStore.getStatus().time;
+
+        VideoControlsComponent.instance().onIncrease();
+        callback(actionIncrease);
+
+        var newTime = VideoStore.getStatus().time;
+        expect(oldTime).toBeGreaterThanOrEqual(newTime);
+	});
+
+    it('should decrease time after backward action clicked', () => {
+        const VideoControlsComponent = shallow(<VideoControls />);
+
+        var oldTime = VideoStore.getStatus().time;
+
+        VideoControlsComponent.instance().onDecrease();
+        callback(actionDecrease);
+
+        var newTime = VideoStore.getStatus().time;
+        expect(oldTime).toBeLessThanOrEqual(newTime);
 	});
 });
