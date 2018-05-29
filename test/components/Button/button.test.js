befde407f8f0258d846d4da732c4d468daee5d0f
Maximo Fassnacht <maximo.fassnacht@exozet.com>
2018-05-29 13:30:16 +0200

    fix test suit

--- test/components/Button/button.test.js
+++ test/components/Button/button.test.js
@@ -1,8 +1,27 @@
 // button.test.js
 import React from 'react';
 import Button from '../../../src/scripts/components/Button/button.jsx';
-import renderer from 'react-test-renderer';
+import { shallow, mount, render } from 'enzyme';
 
-test('Button changes the class when hovered', () => {
+describe('TESTING BUTTON', function() {
 
+	it('should be selectable by class "button"', function() {
+		expect(shallow(<Button />).is('.button')).toBe(true);
+	});
+
+	it('should mount in a full DOM', function() {
+		expect(mount(<Button />).find('.button').length).toBe(1);
+	});
+
+	it('should render to static HTML', function() {
+		expect(render(<Button text="button text"/>).text()).toEqual('button text');
+	});
+
+	it('should trigger the clickHandler when click', () => {
+		const mockCallBack = jest.fn();
+		const button = shallow((<Button text="button text" clickHandler={mockCallBack}></Button>));
+		button.simulate('click');
+		expect(mockCallBack.mock.calls.length).toBe(1);
+		expect(mockCallBack).toHaveBeenCalled();
+	});
 });
